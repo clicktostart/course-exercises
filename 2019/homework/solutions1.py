c441a4ff719fc0f0a3502390cb32224c0eb145e8
Marcus Sanatan <msanatan@gmail.com>
2019-07-10 22:36:54 -0400

    Add solutions to first homework exercise

---
+++ 2019/homework/solutions1.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'Marcus Sanatan'
+__copyright__ = 'Copyright (C) 2019, Click to Start'
+__credits__ = ['Marcus Sanatan', 'Warren O\'Connell']
+__license__ = 'MIT'
+__version__ = '0.0.1'
+
+# Enter your name in this variable
+name = 'Mentor'
+print('This is the homework #1 for {}'.format(name))
+
+# Question 1
+# Print the following sentence: Hello, I am Herbert the chatbot!
+print('Hello, I am Herbert the chatbot!')
+
+
+# Question 2
+# Using the newline character, use ONE print statement to display the following text:
+# Southern trees bear strange fruit,
+# (Blood on the leaves and blood at the root)
+# Black body swinging in the southern breeze,
+# Strange fruit hanging from the poplar trees.
+print('Southern trees bear strange fruit,\n(Blood on the leaves and blood at the root)\nBlack body swinging in the southern breeze,\nStrange fruit hanging from the poplar trees.')
+
+# Question 3
+# Create a variable called "pi" and make it equal to 22 divided by 7
+pi = 22 / 7
+
+# Use the variable to print the following statement (substituting <> for your variable):
+# A decent approximation for the infinite decimal PI is <THE VALUE OF "pi">
+print('A decent approximation for the infinite decimal PI is {}'.format(pi))
+
+# Question 4
+new_houses_oprah_gave_me = 2
+# Oprah is once again in a good mood and gave you 2 new houses. Create a variable
+# called "total_houses_oprah_gave_me" and set it to "new_houses_oprah_gave_me"
+# plus 11 (yes, Oprah gave you 11 houses already. Favourite child?)
+total_houses_oprah_gave_me = new_houses_oprah_gave_me + 11
+
+# Use "total_houses_oprah_gave_me" to print (substituting <> for your variable):
+# Oprah loves me so much she gave me <THE VALUE OF "total_houses_oprah_gave_me"> houses!
+print('Oprah loves me so much she gave me {} houses!'.format(total_houses_oprah_gave_me))
+
+#  Question 5
+days_until_christmas = "168"
+# A month has gone and we want to substract 30 days. The value is a string, so
+# we cannot subtract 30 from it. In a new variable "remaining_days", convert the
+# values of "days_until_christmas" into an integer (whole number) and subtract
+# 30 from it
+remaining_days = int(days_until_christmas) - 30
+
+# Question 6
+# Ask the user: "How many centimetres long is the side of your square?"
+# Save the result in a variable called "side_length"
+side_length = input('How many centimetres long is the side of your square?')
+
+# Create a new variable called "area" that uses "side_length" to determine the
+# area of the square.
+area = int(side_length) * int(side_length)
+
+# Let the user know the area of the square by printing (substituting <> for your variable):
+# The area of your square is <THE VALUE OF "area">
+print('The area of your square is {}'.format(area))
